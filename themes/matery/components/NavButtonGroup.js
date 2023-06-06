347d44af9aa9203338a585dec397552608d1b922
wangyang <wangyang10@mychery.com>
2023-06-06 10:04:31 +0800

    feat: 样式修改

--- themes/matery/components/NavButtonGroup.js
+++ themes/matery/components/NavButtonGroup.js
@@ -1,4 +1,7 @@
-
+/*
+ * @Date: 2023-06-05 16:39:41
+ * @FilePath: \NotionNext\themes\matery\components\NavButtonGroup.js
+ */
 import React from 'react'
 import Link from 'next/link'
 
@@ -7,14 +10,19 @@ import Link from 'next/link'
  * @param {*} props
  * @returns
  */
-const NavButtonGroup = (props) => {
+const NavButtonGroup = props => {
   const { categories } = props
   if (!categories || categories.length === 0) {
     return <></>
   }
 
   return (
-    <nav id='home-nav-button' className={'md:h-52 md:mt-6 xl:mt-32 px-5 py-2 mt-8 flex flex-wrap md:max-w-5xl space-y-2 md:space-y-0 md:flex justify-center max-h-80 overflow-auto'}>
+    <nav
+      id="home-nav-button"
+      className={
+        'md:min-h-52 md:mt-6 xl:mt-32 px-5 py-2 mt-8 flex flex-wrap md:max-w-5xl space-y-2 md:space-y-0 md:flex justify-center max-h-80 overflow-auto'
+      }
+    >
       {categories.map(category => {
         return (
           <Link
@@ -22,9 +30,10 @@ const NavButtonGroup = (props) => {
             title={`${category.name}`}
             href={`/category/${category.name}`}
             passHref
-            className='text-center w-full md:mx-6 md:w-40 md:h-14 lg:h-20 h-14 justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism hover:bg-white hover:text-black duration-200 font-bold hover:scale-110 transform'>
-                {category.name}
-            </Link>
+            className="text-center w-full md:mx-6 md:w-40 md:h-14 lg:h-20 h-14 justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism hover:bg-white hover:text-black duration-200 font-bold hover:scale-110 transform"
+          >
+            {category.name}
+          </Link>
         )
       })}
     </nav>
